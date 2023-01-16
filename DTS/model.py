37b96263565d033e5b6ba7746f5eb9320f44b78f
roy <gkstjd1201@naver.com>
2023-01-16 13:47:26 +0000

    feat : train test 완료!

--- DTS/model.py
+++ DTS/model.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from transformers import get_linear_schedule_with_warmup
 import torch.nn as nn
 import pandas as pd
-from tqdm import tqdm
+from tqdm.auto import tqdm
 from load_dataset import DTSDataset
 
 def MarginRankingLoss(p_scores, n_scores):
@@ -22,7 +22,6 @@ def MarginRankingLoss(p_scores, n_scores):
 device = 0
 # Load the BERT tokenizer.
 print('Loading BERT tokenizer...')
-tokenizer = AutoTokenizer.from_pretrained('klue/bert-base', do_lower_case=True)
 coherence_prediction_decoder = []
 coherence_prediction_decoder.append(nn.Linear(768, 768))
 coherence_prediction_decoder.append(nn.ReLU())
@@ -31,6 +30,7 @@ coherence_prediction_decoder.append(nn.Linear(768, 2))
 coherence_prediction_decoder = nn.Sequential(*coherence_prediction_decoder)
 coherence_prediction_decoder.to(device)
 
+tokenizer = AutoTokenizer.from_pretrained('klue/bert-base', do_lower_case=True)
 model = BertForNextSentencePrediction.from_pretrained("klue/bert-base", num_labels = 2, output_attentions = False, output_hidden_states = True)
 model.cuda(device)
 optimizer = AdamW(list(model.parameters())+list(coherence_prediction_decoder.parameters()), lr = 2e-5, eps = 1e-8)
@@ -43,13 +43,24 @@ id_inputs = []
 #     line = line.strip()
 #     sample_num_memory.append(int(line))
 
-PATH = '/opt/ml/input/data/poc/KakaoTalk_Chat.csv'
+PATH = '/opt/ml/input/data/poc/KakaoTalk_Chat_IT개발자 구직:채용 정보교류방 (비번 2186)_2023-01-11-12-07-28.csv'
 df = pd.read_csv(PATH)
 
 print('The group number is: '+ str(len(df)))
 # generate pos/neg pairs ....
 print('start generating pos and neg pairs ... ')
+# def collate_fn(batch):
+#     print('batch', batch)
+#     data_collator = DataCollatorWithPadding(tokenizer,padding=True, pad_to_multiple_of=8)
+#     # pos_ids, neg_ids = item['input_ids'] for item in batch
+#     pos_mask, neg_mask = batch['attention_mask']
+#     labels = batch['label']
+#     pos_ids = data_collator({'input_ids': pos_ids})['input_ids']
+#     neg_ids = data_collator({'input_ids': neg_ids})['input_ids']
 
+#     return {'input_ids' : [pos_ids, neg_ids],
+#                 'attention_mask' : [pos_mask,neg_mask],
+#                 'label' : labels}
 ####################### 여기부터 미완 ###################################
 # TODO : Sampling 방법 고안해보기
 # 
@@ -73,8 +84,8 @@ train_inputs, validation_inputs, train_labels, validation_labels = train_test_sp
 # Do the same for the masks.
 train_dataset = DTSDataset(train_inputs,tokenizer)
 valid_dataset = DTSDataset(validation_inputs,tokenizer)
-data_collator = DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8)
-
+print(f'train_len : {len(train_dataset)}, valid_len : {len(valid_dataset)} now loading ...')
+data_collator = DataCollatorWithPadding(tokenizer,padding=True, pad_to_multiple_of=8)
 batch_size = 16
 train_sampler = RandomSampler(train_dataset)
 train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size,collate_fn=data_collator)
@@ -104,23 +115,26 @@ for epoch_i in range(0, epochs):
     model.train()
     coherence_prediction_decoder.train()
 
-    for step, batch in tqdm(enumerate(train_dataloader),desc = 'train_step', total = len(train_dataset)):
+    for step, batch in tqdm(enumerate(train_dataloader),desc = 'train_step', total = len(train_dataset)//batch_size):
 
         if step % 1000 == 0 and not step == 0:
             print(str(step)+' steps done....')
 
-        pos_input_ids = batch['pos_input_ids'].to(device)
+        pos_input_ids = batch['input_ids'].to(device)
+        pos_input_mask = batch['attention_mask'].to(device)
         neg_input_ids = batch['neg_input_ids'].to(device)
-        pos_input_mask = batch['pos_attention_mask'].to(device)
         neg_input_mask = batch['neg_attention_mask'].to(device)
+        # pos_input_ids, neg_input_ids = batch['input_ids'][0].to(device), batch['input_ids'][1].to(device)
+        # pos_input_mask, neg_input_mask = batch['attention_mask'][0].to(device), batch['attention_mask'][1].to(device)
+
         model.zero_grad()
         coherence_prediction_decoder.zero_grad()
 
-        pos_scores = model(input_ids = pos_input_ids, attention_mask=pos_input_mask).hidden_states[0]
+        pos_scores = model(input_ids = pos_input_ids, attention_mask=pos_input_mask).hidden_states[0] #batch_size, sequence_length, hidden_size
         pos_scores = pos_scores[:,0,:]
         pos_scores = coherence_prediction_decoder(pos_scores)
 
-        neg_scores = model(input_ids = neg_input_ids, attention_mask=neg_input_mask).hidden_states[0]
+        neg_scores = model(input_ids = neg_input_ids, attention_mask=neg_input_mask).hidden_states[0] #batch_size, sequence_length, hidden_size
         neg_scores = neg_scores[:,0,:]
         neg_scores = coherence_prediction_decoder(neg_scores)
 
@@ -146,15 +160,17 @@ for epoch_i in range(0, epochs):
     all_pos_scores = []
     all_neg_scores = []
 
-    for step, batch in tqdm(enumerate(validation_dataloader),desc = 'validation_step',total = len(valid_dataset)):
+    for step, batch in tqdm(enumerate(validation_dataloader),desc = 'validation_step',total = len(valid_dataset)//batch_size):
 
         if step % 1000 == 0 and not step == 0:
             print(str(step)+' steps done....')
 
-        pos_input_ids = batch['pos_input_ids'].to(device)
+        pos_input_ids = batch['input_ids'].to(device)
+        pos_input_mask = batch['attention_mask'].to(device)
         neg_input_ids = batch['neg_input_ids'].to(device)
-        pos_input_mask = batch['pos_attention_mask'].to(device)
         neg_input_mask = batch['neg_attention_mask'].to(device)
+        # pos_input_ids, neg_input_ids = batch['input_ids'][0].to(device), batch['input_ids'][1].to(device)
+        # pos_input_mask, neg_input_mask = batch['attention_mask'][0].to(device), batch['attention_mask'][1].to(device)
 
         with torch.no_grad():
             pos_scores = model(input_ids = pos_input_ids, attention_mask=pos_input_mask).hidden_states[0]
